758705ef1e43fc819a881519658b3595061f033c
syncdecodes <syncdecodes@gmail.com>
2026-05-25 16:41:12 +0530

    Add comment out notes in generic ptr

--- 07_pointers_memory/06generic_ptr.cpp
+++ 07_pointers_memory/06generic_ptr.cpp
@@ -16,3 +16,14 @@ int main()
     cout << *intPtr << endl;
     return 0;
 }
+
+/*
+Note when we add a number to a pointer -:
+pointer moves by:
+number x size of datatype
+
+so if address of numPtr (pointer to an int of 4 bytes) is 5000.
+and if we do numPtr = numPtr + 3
+then 3 x 4 = 12 
+numPtr = 5000 + 12 = 5012
+*/
