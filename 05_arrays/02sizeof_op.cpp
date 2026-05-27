f1bfc949ea028ab90f7fdedec2fcb5c06a15f145
syncdecodes <syncdecodes@gmail.com>
2026-05-27 13:48:51 +0530

    Update formatting

--- 05_arrays/02sizeof_op.cpp
+++ 05_arrays/02sizeof_op.cpp
@@ -16,12 +16,12 @@ int main()
     char chararr[] = {'a', 'b', 'c', 'd', 'e'};
     std::string students[] = {"spongebob", "patric", "squidward", "sandy"};
 
-    std::cout << sizeof(gpa) << "bytes\n";
-    std::cout << sizeof(name) << "bytes\n";
-    std::cout << sizeof(grade) << "bytes\n";
-    std::cout << sizeof(student) << "bytes\n";
-    std::cout << sizeof(chararr) << "bytes\n";
-    std::cout << sizeof(students) << "bytes\n";
+    std::cout << sizeof(gpa) << " bytes\n";
+    std::cout << sizeof(name) << " bytes\n";
+    std::cout << sizeof(grade) << " bytes\n";
+    std::cout << sizeof(student) << " bytes\n";
+    std::cout << sizeof(chararr) << " bytes\n";
+    std::cout << sizeof(students) << " bytes\n";
 
     return 0;
 }
